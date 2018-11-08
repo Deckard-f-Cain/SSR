33f136d138e2f25edbaba315620d992d0a054ca4
Deckard-f-Cain <44095235+Deckard-f-Cain@users.noreply.github.com>
2018-11-08 11:28:02 +0800

    Update Cain-SSR.sh

--- Cain-SSR.sh
+++ Cain-SSR.sh
@@ -4,7 +4,7 @@ export PATH
 #=================================================================#
 #   System Required:  CentOS 6,7, Debian, Ubuntu                  #
 #   Description: One click Install ShadowsocksR Server            #
-#   Author: Cain <vip_hadji@hotmail.com>                          #
+#   Author: Teddysun <i@teddysun.com>                             #
 #   Thanks: @breakwa11 <https://twitter.com/breakwa11>            #
 #   Intro:  https://shadowsocks.be/9.html                         #
 #=================================================================#
